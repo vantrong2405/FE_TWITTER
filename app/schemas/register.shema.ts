6bd698f65ff810120f33cc2b59a65d47e8b2114b
van trong <trongdn2405@gmail.com>
2025-01-24 00:17:29 +0700

    fix: any bug

--- app/schemas/register.shema.ts
+++ app/schemas/register.shema.ts
@@ -2,7 +2,13 @@ import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { schema, TypeSchema } from './rules'
 
-export type IValidateRegister = Omit<TypeSchema, 'forgot_password_token'>
+export type IValidateRegister = {
+  name: string
+  email: string
+  password: string
+  confirm_password: string
+  date_of_birth: string
+}
 
 export const registerSchema = schema.pick(['email', 'password', 'confirm_password', 'name', 'date_of_birth'])
 
